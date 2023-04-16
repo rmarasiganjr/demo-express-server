99b3bdbefc87ea9898998daad0fad51f3c55f292
rmarasiganjr <44512966+rmarasiganjr@users.noreply.github.com>
2023-04-17 01:15:24 +0800

    base file

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -33,21 +33,18 @@ router.post('/login', (req, res, next) => {
     sqlQuery = `SELECT * FROM account_credentials WHERE email="${email}" AND password="${password}"`;
     dbConn.query(sqlQuery, function (error, results) {
       console.log(results);
-      Object.keys(results).forEach(function (key) {
-        var row = results[key];
+      console.log(process.env.SECRET_TOKEN);
+      // if user does not exist, return error response
+      if (results.length === 0) {
+        return res.status(401).json({ message: 'Invalid credentials' });
+      }
 
-        var data = {
-          username: row.username,
-          email: row.email,
-        };
+      // if user exists, create and sign a JWT
+      const token = jwt.sign({ email }, process.env.SECRET_TOKEN);
 
-        // Create Token
-        token = jwt.sign({ data: data }, process.env.SECRET_TOKEN, {
-          expiresIn: '1h',
-        });
+      // send token back to the client
 
-        res.status(200).json({ success: true, token: token });
-      });
+      res.status(200).json({ success: true, token: token });
     });
   } catch (error) {
     console.log(error);
--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 require('dotenv').config({ path: './.env' });
+const cors = require('cors');
+
 //sample edit for demo
 const app = express();
 app.use(express.json());
+
+// Enable CORS for all routes
+app.use(cors());
 
 const tempRoutes = require('./routes/api/temperature.js');
 const auth = require('./routes/api/auth.js');
@@ -10,8 +15,8 @@ const auth = require('./routes/api/auth.js');
 app.get('/', (req, res) => res.send('API Running, No Error'));
 app.use(express.json({ extend: false }));
 
-app.use('/temperature', tempRoutes);
-app.use('/auth', auth);
+app.use('/api/temperature', tempRoutes);
+app.use('/api/auth', auth);
 
 const PORT = 5000;
 
